74eee8940e09585d8e3e4077445c00232d63b7ca
Nguyễn Thành Trung <trungff07@gmail.com>
2025-11-07 08:51:51 +0700

    add login and get all user

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,18 @@
 import sqlite3
 
+def init_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            name TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,54 @@
 from fastapi import FastAPI
 from model import UserSignUp, User
+from database import get_db_connection, init_db
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {'data' : 'test'}
 
+@app.on_event('startup')
+def startup():
+    init_db()
+
+
 @app.post("/signup")
 def signup(user: UserSignUp):
-    pass
+    connect = get_db_connection()
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ?", (user.username,))
+    existing_user = cursor.fetchone()
+    if existing_user:
+        connect.close()
+        return {'message': 'Username already exists'}
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (user.username, user.password))
+    connect.commit()
+    
+@app.post("/login")
+def login(user: User):
+    connect = get_db_connection()
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (user.username, user.password))
+    existing_user = cursor.fetchone()
+    connect.close()
+    if existing_user:
+        return {'message': 'Login successful'}
+    else:
+        return {'message': 'Invalid username or password'}
+
+@app.get("/users")
+def get_users():
+    connect = get_db_connection()
+    cursor = connect.cursor()
+    cursor.execute("SELECT username, name FROM users")
+    users = cursor.fetchall()
+    connect.close()
+    return {'users': [dict(user) for user in users]}
